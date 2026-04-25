20cd7c174eeb8605328508f0cacab0097b584a6a
dipu123-gupta <kumardipu845420@gmail.com>
2026-04-25 22:17:12 +0530

    kaghsigd

--- server/server.js
+++ server/server.js
@@ -52,10 +52,10 @@ app.use('/api', sessionRoutes);
 
 // Serve Static Frontend in Production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, 'dist')));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
     });
 }
 
